e4785f3773e08f8ff0dacefbe169022d66e95734
Raaaaphh <raphmangini@gmail.com>
2024-05-07 10:34:01 +0200

    Connextion entre Back et Front

---
+++ backend/dist/auth/auth.service.d.ts
@@ -0,0 +1,3 @@
+export declare class AuthService {
+    test(): string;
+}
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  fetch("http://localhost:3000/auth/test")
+    .then(async (res) => {
+      if (!res.ok) {
+        throw new Error();
+      }
+      const textResponse = await res.text();
+      console.log(textResponse);
+    })
+    .catch(() => {
+      console.log("Error");
+    });
   return (
     <div className="App">
       <header className="App-header">
