be26a8a7f43cbea02038d7713bca534e41c01ba3
Raaaaphh <raphmangini@gmail.com>
2024-05-09 10:20:10 +0200

    Modification du script de la base de données

--- data/creation.sql
+++ data/creation.sql
@@ -1,34 +1,29 @@
 drop if exists Favorite;
+
 drop if exists QuestionTag;
+
 drop if exists Tag;
+
 drop if exists Answer;
+
 drop if exists Question;
-drop if exists Lecturer;
-drop if exists Student;
 
-Create table `Student` (
-    `idStu` varchar(255) not null,
+drop if exists User;
+
+Create table `User` (
+    `idUser` varchar(255) not null,
     `name` varchar(255) not null,
     `email` varchar(255) not null,
     `password` varchar(255) not null,
+    `lecturer` boolean default false,
     `createdAt` datetime not null,
     `updatedAt` datetime not null,
-    primary key (`idStu`)
-);
-
-Create table `Lecturer` (
-    `idLect` varchar(255) not null,
-    `name` varchar(255) not null,
-    `email` varchar(255) not null,
-    `password` varchar(255) not null,
-    `createdAt` datetime not null,
-    `updatedAt` datetime not null,
-    primary key (`idLect`)
+    primary key (`idUser`)
 );
 
 Create table `Question` (
     `idQuest` varchar(255) not null,
-    `idStu` varchar(255) not null,
+    `idUser` varchar(255) not null,
     `content` text not null,
     `votes` int default 0,
     `flagsSpam` int default 0,
@@ -36,46 +31,42 @@ Create table `Question` (
     `status` enum('Solved', 'Unsolved') default 'Unsolved',
     `createdAt` datetime not null,
     `updatedAt` datetime not null,
-    primary key (`idQuest`)
-    foreign key (`idStu`) references `Student`(`idStu`) ON DELETE CASCADE,
+    primary key (`idQuest`) foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
 );
 
-Create table `Answer`(
+Create table `Answer` (
     `idAnsw` varchar(255) not null,
     `idQuest` varchar(255) not null,
-    `idLect` varchar(255) not null,
+    `idUser` varchar(255) not null,
     `content` text not null,
     `final` boolean default false,
     `createdAt` datetime not null,
     `updatedAt` datetime not null,
-    primary key (`idAns`)
-    foreign key (`idQuest`) references `Question`(`idQuest`) ON DELETE CASCADE,
-    foreign key (`idLect`) references `Lecturer`(`idLect`) ON DELETE CASCADE,
+    primary key (`idAns`) foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE,
+    foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
 );
 
 Create table `Tag` (
     `idTag` varchar(255) not null,
-    `idLect` varchar(255) not null,
+    `idUser` varchar(255) not null,
     `name` varchar(255) not null,
     `description` text not null,
+    `occurrence` int default 0,
     `createdAt` datetime not null,
     `updatedAt` datetime not null,
-    primary key (`idTag`)
-    foreign key (`idLect`) references `Lecturer`(`idLect`) ON DELETE CASCADE,
+    primary key (`idTag`) foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
 );
 
 Create table `QuestionTag` (
     `idQuest` varchar(255) not null,
     `idTag` varchar(255) not null,
-    primary key (`idQuest`, `idTag`)
-    foreign key (`idQuest`) references `Question`(`idQuest`) ON DELETE CASCADE,
-    foreign key (`idTag`) references `Tag`(`idTag`) ON DELETE CASCADE,
+    primary key (`idQuest`, `idTag`) foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE,
+    foreign key (`idTag`) references `Tag` (`idTag`) ON DELETE CASCADE,
 );
 
 Create table `Favorite` (
-    `idStu` varchar(255) not null,
+    `idUser` varchar(255) not null,
     `idQuest` varchar(255) not null,
-    primary key (`idStu`, `idQuest`)
-    foreign key (`idStu`) references `Student`(`idStu`) ON DELETE CASCADE,
-    foreign key (`idQuest`) references `Question`(`idQuest`) ON DELETE CASCADE,
+    primary key (`idUser`, `idQuest`) foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
+    foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE,
 );
