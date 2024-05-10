a798a453876d06db96a5289484dc13e905575608
Raaaaphh <raphmangini@gmail.com>
2024-05-10 03:00:10 +0200

    Modification script bdd

--- data/creation.sql
+++ data/creation.sql
@@ -11,7 +11,7 @@ drop if exists Question;
 drop if exists User;
 
 Create table `User` (
-    `idUser` varchar(255) not null,
+    `idUser` varchar(100) not null,
     `name` varchar(255) not null,
     `email` varchar(255) not null,
     `password` varchar(255) not null,
@@ -22,8 +22,8 @@ Create table `User` (
 );
 
 Create table `Question` (
-    `idQuest` varchar(255) not null,
-    `idUser` varchar(255) not null,
+    `idQuest` varchar(100) not null,
+    `idUser` varchar(100) not null,
     `content` text not null,
     `votes` int default 0,
     `flagsSpam` int default 0,
@@ -31,42 +31,47 @@ Create table `Question` (
     `status` enum('Solved', 'Unsolved') default 'Unsolved',
     `createdAt` datetime not null,
     `updatedAt` datetime not null,
-    primary key (`idQuest`) foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
+    primary key (`idQuest`),
+    foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE
 );
 
 Create table `Answer` (
-    `idAnsw` varchar(255) not null,
-    `idQuest` varchar(255) not null,
-    `idUser` varchar(255) not null,
+    `idAnsw` varchar(100) not null,
+    `idQuest` varchar(100) not null,
+    `idUser` varchar(100) not null,
     `content` text not null,
     `final` boolean default false,
     `createdAt` datetime not null,
     `updatedAt` datetime not null,
-    primary key (`idAns`) foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE,
-    foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
+    primary key (`idAnsw`),
+    foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE,
+    foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE
 );
 
 Create table `Tag` (
-    `idTag` varchar(255) not null,
-    `idUser` varchar(255) not null,
+    `idTag` varchar(100) not null,
+    `idUser` varchar(100) not null,
     `name` varchar(255) not null,
     `description` text not null,
     `occurrence` int default 0,
     `createdAt` datetime not null,
     `updatedAt` datetime not null,
-    primary key (`idTag`) foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
+    primary key (`idTag`),
+    foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE
 );
 
 Create table `QuestionTag` (
-    `idQuest` varchar(255) not null,
-    `idTag` varchar(255) not null,
-    primary key (`idQuest`, `idTag`) foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE,
-    foreign key (`idTag`) references `Tag` (`idTag`) ON DELETE CASCADE,
+    `idQuest` varchar(100) not null,
+    `idTag` varchar(100) not null,
+    primary key (`idQuest`, `idTag`),
+    foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE,
+    foreign key (`idTag`) references `Tag` (`idTag`) ON DELETE CASCADE
 );
 
 Create table `Favorite` (
-    `idUser` varchar(255) not null,
-    `idQuest` varchar(255) not null,
-    primary key (`idUser`, `idQuest`) foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
-    foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE,
+    `idUser` varchar(100) not null,
+    `idQuest` varchar(100) not null,
+    primary key (`idUser`, `idQuest`),
+    foreign key (`idUser`) references `User` (`idUser`) ON DELETE CASCADE,
+    foreign key (`idQuest`) references `Question` (`idQuest`) ON DELETE CASCADE
 );
