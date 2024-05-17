0d7d77ba7c3376850457784c4eabdbf40e6ce0cb
benjamin.brajon@etu.univ-lyon1.fr <benjamin.brajon@etu.univ-lyon1.fr>
2024-05-17 16:23:59 +0800

    profile page user detail section no route

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home';
 import NotFound from './pages/NotFound';
 import AskAQuestion from './pages/AskAQuestion';
 import AuthForm from './pages/AuthForm';
+import Profile from './pages/Profile';
 
 export const Routes = () => {
     return (
@@ -12,7 +13,8 @@ export const Routes = () => {
                 <Route path="*" element={<NotFound/>}/>
                 <Route path="/login" element={<AuthForm />} />
                 <Route path="/register" element={<AuthForm isRegister />} />
-                <Route path="question" element={<AskAQuestion/>}/>
+                <Route path="/question" element={<AskAQuestion/>}/>
+                <Route path="/profile" element={<Profile/>}/>
             </RoutesComponents>
         </Router>
     );
